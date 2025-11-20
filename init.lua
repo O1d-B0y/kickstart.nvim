28ee0adfb5169df75b0db7d16bb0e103ba1e0c3f
O1d-b0y <t.s.davis6291@gmail.com>
2025-11-19 21:11:42 -0600

    Fixed issue with mini plugins (remove statusline, add surround)

--- init.lua
+++ init.lua
@@ -611,12 +611,7 @@ require('lazy').setup({
             -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
             -- - sd'   - [S]urround [D]elete [']quotes
             -- - sr)'  - [S]urround [R]eplace [)] [']
-            statusline.setup { use_icons = vim.g.have_nerd_font }
-            ---@diagnostic disable-next-line: duplicate-set-field
-            statusline.section_location = function()
-                return '%2l:%-2v'
-            end
-
+            require 'mini.surround'
             -- ... and there is more!
             --  Check out: https://github.com/echasnovski/mini.nvim
         end,
